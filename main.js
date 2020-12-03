fee96ab4822409b1c704a8ff293f7d579ba03dfa
Austin Corwin <austincorwin5@gmail.com>
2020-12-03 09:33:45 -0700

    Added JS and built out beginning functionality

--- main.js
+++ main.js
@@ -1,1 +1,51 @@
-console.log('hello');
+
+const calcButtons = document.querySelectorAll('button');
+calcButtons.forEach(calcButton => calcButton.addEventListener('click', function(e){
+    let buttonValue = calcButton.innerHTML;
+    let currentOperand = document.querySelector('.current-operand');
+
+    if (buttonValue == 'AC'){
+        currentOperand.textContent = '';
+
+    } else if (buttonValue == 'DEL') {
+        currentOperandText = 
+
+        currentOperand.textContent = 'deletedstring';
+    }
+    
+    
+    
+    else {
+    currentOperand.textContent += buttonValue;
+    console.log(buttonValue);
+    };
+}))
+
+
+
+
+// console.log(calcButtons);
+
+
+
+// Console Log the value of each of the buttons from the node list
+
+// calcButtons.forEach(function getButtonValue(){
+
+//     for (i=0; i<calcButtons.length; i++){
+//     buttonValue = calcButtons[i].innerHTML;
+//     console.log(buttonValue);
+// }
+
+// })
+
+// calcButtons.forEach(function(){
+//     calcButtons.addEventListener('click',function(e){
+//         let buttonValue = calcButtons[0].innerHTML;
+//         console.log(buttonValue);
+//     })
+// })
+
+
+
+// from stack overflow
